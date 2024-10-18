72d1bab050e8cbbf938a6d0c021e84a64dffec8e
dbesong <ashu.emerence@yahoo.com>
2024-10-18 00:35:27 -0600

    updated backend

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 
 
 terraform {
-  required_version = "<=1.9.7"
+  required_version = ">=1.0.0" #required_version = "<=1.9.7"
   backend "s3" {
     key            = "global/dev/eks-cluster/terraform.tfstate"
     bucket         = "lmt-training-terraform-backend-bucket"
